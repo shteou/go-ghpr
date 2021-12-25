1d863b6743e6c9dfdb1bfe707384679834858cef
Stewart Platt <shteou@gmail.com>
2021-12-25 10:26:09 +0000

    test: add PR url tests (#22)

--- pkg/ghpr/pr_test.go
+++ pkg/ghpr/pr_test.go
@@ -1,1 +1,34 @@
 package ghpr
+
+import (
+	"testing"
+
+	"github.com/go-git/go-billy/v5/memfs"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/stretchr/testify/assert"
+)
+
+func dummyFunc(w *git.Worktree) (string, *object.Signature, error) {
+	return "", nil, nil
+}
+
+func TestPRUrlNoPrNumber(t *testing.T) {
+	repo := newRepo("test", "user", memfs.New(), &mockGoGit{})
+	change := NewChange(repo, "test", Credentials{}, dummyFunc)
+	pr := newPR(change, nil)
+
+	_, err := pr.URL()
+	assert.NotNil(t, err)
+}
+
+func TestPRUrl(t *testing.T) {
+	repo := newRepo("test", "user", memfs.New(), &mockGoGit{})
+	change := NewChange(repo, "test", Credentials{}, dummyFunc)
+	pr := newPR(change, nil)
+	pr.Number = 1
+
+	url, err := pr.URL()
+	assert.Nil(t, err)
+	assert.Equal(t, "https://github.com/test/user/pulls/1", url)
+}
